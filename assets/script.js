605ad5f3a139836373c4cc949490f2fa01dd7ba7
Jules O'Neale <morticiousplays@gmail.com>
2021-12-16 20:34:35 -0700

    Killed AJAX code, rewrote .js file, now using variables and Fetch

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,13 @@
 // The stored API key for the Weather Application
 // bd6845ddedcbf1291ba7ad3277418285
+// Used in API OpenWeather Map, parameter appid
+var APIKey = "bd6845ddedcbf1291ba7ad3277418285";
 
-const form = document.querySelector(".top-banner form");
-const input = document.querySelector(".top-banner input");
-const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
+// The city requested by the user; also counts as the state and/or country code
+// Used in API OpenWeather Map, parameter q
+var city;
 
-const apiKey = "bd6845ddedcbf1291ba7ad3277418285";
+// Concatenation of URL to the OpenWeather Map API, includes city and APIKey
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const inputVal = input.value;
-
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${
-        weather[0]["icon"]
-      }@2x.png`;
-
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Please search for a valid city :(";
-    });
-
-  msg.textContent = "";
-  form.reset();
-  input.focus();
-});
+fetch(queryURL)
