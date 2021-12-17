c9b48320b2c7bd8867b3065c2b42a01d7654bf38
Jules O'Neale <morticiousplays@gmail.com>
2021-12-16 18:32:40 -0700

    add index.html

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,44 @@ form.addEventListener("submit", e => {
 const apiKey = "bd6845ddedcbf1291ba7ad3277418285";
 const inputVal = input.value;
 
-...
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey};
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // do stuff with the data
+  })
+  .catch(() => {
+    msg.textContent = "Please search for a valid city :(";
+  });
+
+
+  // list item component!
+  const { main, name, sys, weather } = data;
+const icon = `https://openweathermap.org/img/wn/${
+  weather[0]["icon"]
+}@2x.png`;
+
+const li = document.createElement("li");
+li.classList.add("city");
+const markup = `
+  <h2 class="city-name" data-name="${name},${sys.country}">
+    <span>${name}</span>
+    <sup>${sys.country}</sup>
+  </h2>
+  <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+  </div>
+  <figure>
+    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+    <figcaption>${weather[0]["description"]}</figcaption>
+  </figure>
+`;
+
+// Resets the search bar
+li.innerHTML = markup;
+list.appendChild(li);
+
+msg.textContent = "";
+form.reset();
+input.focus();
