096be7445d830ad75d781a5ae7355f41a6d8395c
Jules O'Neale <morticiousplays@gmail.com>
2021-12-16 18:24:21 -0700

    add index.html

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 // The stored API key for the Weather Application
-var APIKey = "bd6845ddedcbf1291ba7ad3277418285";
+// bd6845ddedcbf1291ba7ad3277418285
 
-var city;
-var stateCode;
-var countryCode;
+// This code is designed so that when the 'Enter' key or Submit button is pressed, the form is stopped from submitting, and it grabs the value in the search field.
+const form = document.querySelector(".top-banner form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputVal = input.value;
+});
+
+// AJAX Request code
+const apiKey = "bd6845ddedcbf1291ba7ad3277418285";
+const inputVal = input.value;
+
+...
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiK
